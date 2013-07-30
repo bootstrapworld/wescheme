ba2bd57354a9bb644ae8386f249461e930653591
David Percy <dpercy@cs.brown.edu>
2013-07-30 00:51:51 -0400

    updated Google API config to point to wescheme-dpercy to make the image picker work on wescheme-dpercy.appspot.com.

--- war-src/js/openEditor/config.js
+++ war-src/js/openEditor/config.js
@@ -1,4 +1,4 @@
 goog.provide('plt.config');
 
-plt.config.APP_ID = "united-blend-281";    // The app ID associated with the App Engine project
-plt.config.CLIENT_ID = "979522551881-h4kj3gi4p7bm93jb9asec6i807tudps2.apps.googleusercontent.com";   // The Client key associated with the Google APIs project
+plt.config.APP_ID = "wescheme-dpercy";    // The app ID associated with the App Engine project
+plt.config.CLIENT_ID = "152920516244.apps.googleusercontent.com"   // The Client key associated with the Google APIs project
