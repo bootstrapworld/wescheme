6879dc4b6dd8a2b7b305fbdf2dfb69444d510639
David Percy <dpercy@cs.brown.edu>
2013-08-08 17:58:32 -0400

    fixed the rebuild-and-upload script

--- rebuild-and-upload.sh
+++ rebuild-and-upload.sh
@@ -2,4 +2,5 @@
 set -e
 ./build-console-and-editor.rkt
 ./copy-compiler-libraries.sh
-./lib/appengine-java-sdk-1.7.3/bin/appcfg.sh update war
+##./lib/appengine-java-sdk-1.7.3/bin/appcfg.sh update war
+ant update
