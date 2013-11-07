8eecdebaf1e9ffdc84954a9de71ca2a6819161db
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2013-11-07 08:44:33 -0800

    fix indenting on statusbar code (no substantive changes)

--- war-src/js/openEditor/statusbar.js
+++ war-src/js/openEditor/statusbar.js
@@ -17,45 +17,43 @@ var WeSchemeStatusBar;
 
 plt.wescheme.WeSchemeStatusBar = WeSchemeStatusBar = (function() {
     function WeSchemeStatusBar(statusbar) {
-	this.statusbar = statusbar;
+    this.statusbar = statusbar;
 
-	this.delay_till_fade = 5000; // five seconds until we fade the text.
-	this.fadeCallbackId = undefined;
+    this.delay_till_fade = 5000; // five seconds until we fade the text.
+    this.fadeCallbackId = undefined;
 
-	var that = this;
-	var handleNotifyIntent = function(action, category, data) {
+    var that = this;
+    var handleNotifyIntent = function(action, category, data) {
 	    var editorNotifyCategoryMap = {
-		'before-save': 'Program is being saved',
-		'after-save': 'Program has been saved',
-		'before-clone': 'Program is being cloned',
-		'after-clone': 'Program has been cloned',
-		'before-load': 'Program is being loaded',
-		'after-load': 'Program has been loaded',
-		'before-publish': 'Program is being published',
-		'after-publish': 'Program has been published',
-		'before-run': 'Program is being executed',
-		'after-run': 'Program has been executed',
-		'before-share': 'Program is being shared',
-		'after-share': 'Program has been shared',
-                'before-editor-reload-on-save': 'Please wait; saving program...'
-            };
+        'before-save': 'Program is being saved',
+        'after-save': 'Program has been saved',
+        'before-clone': 'Program is being cloned',
+        'after-clone': 'Program has been cloned',
+        'before-load': 'Program is being loaded',
+        'after-load': 'Program has been loaded',
+        'before-publish': 'Program is being published',
+        'after-publish': 'Program has been published',
+        'before-run': 'Program is being executed',
+        'after-run': 'Program has been executed',
+        'before-share': 'Program is being shared',
+        'after-share': 'Program has been shared',
+        'before-editor-reload-on-save': 'Please wait; saving program...'
+      };
 	    if (action === 'notify' &&
-		editorNotifyCategoryMap[category]) {
-		that.notify(editorNotifyCategoryMap[category]);
-	    } else if (action === 'notify' &&
-		       category === 'exception') {
-		var component = data[0];
-		if (component instanceof WeSchemeEditor) {
-		    var operation = data[1];
-		    var statustext = data[2];
-		    var exceptionObj = data[3];
-		    that.notify("Exception occured in operation " + operation);
-		}
-	    }
-	};
-
-	plt.wescheme.WeSchemeIntentBus.addNotifyListener(handleNotifyIntent);
-    }
+          editorNotifyCategoryMap[category]) {
+          that.notify(editorNotifyCategoryMap[category]);
+	    } else if (action === 'notify' && category === 'exception') {
+        var component = data[0];
+        if (component instanceof WeSchemeEditor) {
+            var operation = data[1];
+            var statustext = data[2];
+            var exceptionObj = data[3];
+            that.notify("Exception occured in operation " + operation);
+        }
+      }
+    };
+    plt.wescheme.WeSchemeIntentBus.addNotifyListener(handleNotifyIntent);
+  }
 
 
     var isBlinking = false;
@@ -80,42 +78,40 @@ plt.wescheme.WeSchemeStatusBar = WeSchemeStatusBar = (function() {
         toggle();
         var intervalId =
         setInterval(function() {
-                toggle();
-                if (index > 1) {
-                    // reset
-                    that.statusbar.parent().css("background", oldBackground);
-                    that.statusbar.css("color", oldTextcolor);
-                    isBlinking = false;
-                    clearInterval(intervalId); 
-                }
-            }, 1000);
+                      toggle();
+                      if (index > 1) {
+                          // reset
+                          that.statusbar.parent().css("background", oldBackground);
+                          that.statusbar.css("color", oldTextcolor);
+                          isBlinking = false;
+                          clearInterval(intervalId); 
+                      }
+                    }, 1000);
     };
 
 
     WeSchemeStatusBar.prototype.notify = function(msg) {
-	var that = this;
-	
-	if (this.fadeCallbackId) {
-	    clearTimeout(this.fadeCallbackId);
-	    this.fadeCallbackId = undefined;
-	}
+      var that = this;
+      
+      if (this.fadeCallbackId) {
+          clearTimeout(this.fadeCallbackId);
+          this.fadeCallbackId = undefined;
+      }
 
-        if (msg) { this.catchAttention(); }
+      if (msg) { this.catchAttention(); }
         
-	this.statusbar.text(msg);
-	this.statusbar.fadeIn("fast");
+      this.statusbar.text(msg);
+      this.statusbar.fadeIn("fast");
 
-	this.fadeCallbackId = setTimeout(
-	    function() {
-		that.statusbar.fadeOut(
-                    "fast", function () { that.statusbar.text(""); });
-	    }, 
-	    this.delay_till_fade);
-	// FIXME: make transparent after a while.
-	// FIXME: use flapjax to guarantee that a message shows up for some period of time.
-	// FIXME: allow the user to see all the statusbar messages sent to us.
+      this.fadeCallbackId = setTimeout(
+          function() {
+        that.statusbar.fadeOut("fast", function () { that.statusbar.text(""); });
+          }, 
+          this.delay_till_fade);
+      // FIXME: make transparent after a while.
+      // FIXME: use flapjax to guarantee that a message shows up for some period of time.
+      // FIXME: allow the user to see all the statusbar messages sent to us.
     };
-
 
     return WeSchemeStatusBar;
 })();
