376b1830fc1751276d0a137a9ac97456f532ebe4
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2014-09-01 21:39:23 -0400

    make sure we're using the right clientID

--- war-src/js/openEditor/config.js
+++ war-src/js/openEditor/config.js
@@ -1,4 +1,4 @@
 goog.provide('plt.config');
 
 plt.config.APP_ID = "wescheme";    // The app ID associated with the App Engine project
-plt.config.CLIENT_ID = "1077667753295.apps.googleusercontent.com"   // The Client key associated with the Google APIs project
+plt.config.CLIENT_ID = "900483279628-lpbjh0vro7oub29asb9n6b8atojfjo8k.apps.googleusercontent.com"   // The Client key associated with the Google APIs project
