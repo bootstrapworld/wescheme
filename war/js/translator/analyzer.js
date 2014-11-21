f61737df3ff5a83a4b0b061bfefa5d2254028be0
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2014-11-21 07:22:03 -0500

    not sure why this isnt taking...

--- war/js/translator/analyzer.js
+++ war/js/translator/analyzer.js
@@ -803,7 +803,7 @@ plt.compiler = plt.compiler || {};
  // FIXME: Danny says that using a basePInfo is almost certainly a bug, but we're going to do it for now
  // to match the behavior in Moby, which promotes any closed variables to a global.
  lambdaExpr.prototype.analyzeUses = function(pinfo, env){
-//    var env1 = pinfo.env, // FIXME: this is what the line *should* be
+//    var env1 = pinfo.env, // FIXME: this is what the line *should* be 
     var env1 = plt.compiler.getBasePinfo("base").env,
         env2 = this.args.reduce(function(env, arg){
           return env.extend(new constantBinding(arg.val, false, [], arg.location));
