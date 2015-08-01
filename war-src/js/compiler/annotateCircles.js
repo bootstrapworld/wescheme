76a96be7c4319c4af9914c1ac4e404e893139e4c
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2015-08-01 12:11:47 +0300

    drag and drop starting to work

--- war-src/js/compiler/annotateCircles.js
+++ war-src/js/compiler/annotateCircles.js
@@ -81,6 +81,7 @@ plt.compiler = plt.compiler || {};
  // programToCircles : Listof Programs CM -> Listof DOM nodes
  // assign widgets representing circles of evaluation to tokens in CM
  function programToCircles(programs, cm){
+    cm.getAllMarks().filter(function(m){return m._circles;}).forEach(function(m){m.clear()});
     // 1) convert the AST to a list of DOM trees
     var circles = programs.map(function(p){
       var dom = p.toCircles(cm);
@@ -103,30 +104,82 @@ plt.compiler = plt.compiler || {};
  
     function startEdit(e){
       var node = e.target || e.srcElement;
+      node.contentEditable = "true";
       node.addEventListener("keydown", function(e){
-        if (e.which===13){ node.blur(); e.preventDefault(); }
+        if (e.which===13){ node.contentEditable = "false"; node.blur(); e.preventDefault(); }
       });
       e.preventDefault();
+      node.focus();
     }
  
     function saveEdit(e){
       var node = e.target || e.srcElement;
       node.removeEventListener("keyup");
-      console.log('changing value to '+node.innerText);
       var from = cm.posFromIndex(node.location.startChar),
           to   = cm.posFromIndex(node.location.endChar);
-      cm.replaceRange(node.innerHTML, from, to);
+      cm.replaceRange(node.innerText, from, to);
+    }
+ 
+    cm.dragSrcEl = null;
+ 
+    // make draggable things translucent
+    function handleDragStart(e) {
+      this.style.opacity = '0.4';  // this / e.target is the source node.
+      cm.dragSrcEl = this;
+      var from = cm.posFromIndex(this.location.startChar),
+          to   = cm.posFromIndex(this.location.endChar);
+      e.dataTransfer.effectAllowed = 'move';
+      e.dataTransfer.setData('text', cm.getRange(from, to));
+      console.log('dragging: '+(e.dataTransfer.getData('text')));
+    }
+    function handleDragOver(e) {
+      if (e.preventDefault) { e.preventDefault(); }
+      e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+      return false;
+    }
+
+    // add/remove outlines from the droptargets
+    function handleDragEnter(e) {
+      if(!cm.dragSrcEl.contains(this)) this.classList.add('over');
+    }
+    function handleDragLeave(e) { this.classList.remove('over'); }
+ 
+    // if we've dropped one eleent into a different one, perform the CM modifications
+    function handleDrop(e) {
+      if (e.stopPropagation) { e.stopPropagation(); }
+      if (cm.dragSrcEl != this) {
+        var destFrom = cm.posFromIndex(this.location.startChar),
+            destTo   = cm.posFromIndex(this.location.endChar),
+            srcFrom  = cm.posFromIndex(cm.dragSrcEl.location.startChar),
+            srcTo   = cm.posFromIndex(cm.dragSrcEl.location.endChar);
+        cm.replaceRange(e.dataTransfer.getData('text'), destFrom, destTo);
+        cm.replaceRange('', srcFrom, srcTo);
+      }
+      return false;
     }
  
     // 4) assign behaviors to all editable nodes
     [ ].forEach.call(values_, function (span) {
        span.addEventListener("click", startEdit);
        span.addEventListener("blur", saveEdit);
-       span.addEventListener("dragenter", function(e){console.log(e);});
+       span.addEventListener("dragstart", handleDragStart, false);
+       span.addEventListener("dragover",  handleDragOver,  false);
+       span.addEventListener("dragenter", handleDragEnter, false);
+       span.addEventListener("dragleave", handleDragLeave, false);
+       span.addEventListener("drop",      handleDrop,      false);
     });
     [ ].forEach.call(operators_, function (span) {
        span.addEventListener("click", startEdit);
        span.addEventListener("blur", saveEdit);
+       span.addEventListener("dragstart", handleDragStart, false);
+       span.addEventListener("dragover",  handleDragOver,  false);
+       span.addEventListener("dragenter", handleDragEnter, false);
+       span.addEventListener("dragleave", handleDragLeave, false);
+       span.addEventListener("drop",      handleDrop,      false);
+    });
+    [ ].forEach.call(circles_, function (span) {
+       span.addEventListener("dragstart", handleDragStart, false);
+       span.addEventListener("dragover",  handleDragOver,  false);
     });
  
  }
@@ -161,7 +214,6 @@ plt.compiler = plt.compiler || {};
         endPos = cm.posFromIndex(this.location.endChar);
     expression.className = "circleevalsexp";
     operator.className = "operator";
-    operator.contentEditable = true;
     lParen.className = "lParen";
     rParen.className = "rParen";
     lParen.appendChild(document.createTextNode(cm.getTokenAt(startPos).string));
@@ -184,7 +236,6 @@ plt.compiler = plt.compiler || {};
         endPos = cm.posFromIndex(this.location.endChar);
     expression.className = "circleevalsexp";
     operator.className = "operator";
-    operator.contentEditable = true;
     lParen.className = "lParen";
     rParen.className = "rParen";
     lParen.appendChild(document.createTextNode(cm.getTokenAt(startPos).string));
@@ -194,6 +245,7 @@ plt.compiler = plt.compiler || {};
     expression.appendChild(operator);
     this.exprs.forEach(function(arg){ expression.appendChild(arg.toCircles(cm)); });
     expression.appendChild(rParen);
+    expression.draggable="true";
     expression.location = this.location;
     return expression;
  };
@@ -206,7 +258,6 @@ plt.compiler = plt.compiler || {};
         endPos = cm.posFromIndex(this.location.endChar);
     expression.className = "circleevalsexp";
     operator.className = "operator";
-    operator.contentEditable = true;
     lParen.className = "lParen";
     rParen.className = "rParen";
     lParen.appendChild(document.createTextNode(cm.getTokenAt(startPos).string));
@@ -216,6 +267,7 @@ plt.compiler = plt.compiler || {};
     expression.appendChild(operator);
     this.exprs.forEach(function(arg){ expression.appendChild(arg.toCircles(cm)); });
     expression.appendChild(rParen);
+    expression.draggable="true";
     expression.location = this.location;
     return expression;
  };
@@ -223,9 +275,9 @@ plt.compiler = plt.compiler || {};
 //    var tok = cm.getTokenAt(cm.posFromIndex(this.location.endChar));
     var node = document.createElement('span');
     node.className = "value";
-    node.contentEditable = true;
     node.appendChild(document.createTextNode(this.val));
     if(this.val === "true" || this.val === "false") node.className+=" wescheme-boolean";
+    node.draggable="true";
     node.location = this.location;
     return node;
  };
@@ -235,17 +287,17 @@ plt.compiler = plt.compiler || {};
     var node = document.createElement('span');
     // if it's Not A Number, assume it's a string. Otherwise, number.
     node.className = "value " + (isNaN(this.val)? "wescheme-string" : "wescheme-number");
-    node.contentEditable = true;
     node.appendChild(document.createTextNode(this.toString()));
     node.location = this.location;
+    node.draggable="true";
     return node;
  };
  jsnums.Rational.prototype.toCircles = function(cm){
     var node = document.createElement('span');
     node.className = "value wescheme-number";
-    node.contentEditable = true;
     node.appendChild(document.createTextNode(this.toString(cm)));
     node.location = this.location;
+    node.draggable="true";
     return node;
  };
  jsnums.BigInteger.prototype.toCircles = jsnums.Rational.prototype.toCircles;
@@ -254,9 +306,9 @@ plt.compiler = plt.compiler || {};
  Char.prototype.toCircles = function(cm){
     var node = document.createElement('span');
     node.className = "value wescheme-character";
-    node.contentEditable = true;
     node.appendChild(document.createTextNode(this.toString()));
     node.location = this.location;
+    node.draggable="true";
     return node;
  };
  
