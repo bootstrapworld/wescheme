b7d21362351e8017611a89b030d65b1df66d487f
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2015-08-05 07:30:23 -0400

    cmd-+/cmd-- will now change the font size of the editor, not zoom the whole screen

--- war-src/js/openEditor/topkeymap.js
+++ war-src/js/openEditor/topkeymap.js
@@ -8,6 +8,8 @@ goog.require('plt.wescheme.BrowserDetect');
 var F5_KEYCODE = 116
 var BACKSPACE_KEYCODE = 8;
 var SAVE_KEYCODE = 83;
+var ZOOMIN_KEYCODE = 187;
+var ZOOMOUT_KEYCODE = 189;
 
 
 // Global state: checks to see whether or not we're in the middle of a
@@ -18,65 +20,72 @@ var inMiddleOfSave = false;
 //The following is a global keyhandler that's intended to be attached
 //to the document body, so it should see all key events.
 plt.wescheme.topKeymap = function(e) {
-
     if (e.keyCode === SAVE_KEYCODE &&
-	(plt.wescheme.BrowserDetect.OS==="Mac" ? e.metaKey : e.ctrlKey)) {
+        (plt.wescheme.BrowserDetect.OS==="Mac" ? e.metaKey : e.ctrlKey)) {
 
         if (! inMiddleOfSave) {
             inMiddleOfSave = true;
-	    myEditor.save(
-                function() { 
-                    // successful save
-                    inMiddleOfSave = false; 
-                },
-                function() {
-                    // failed save
-                    inMiddleOfSave = false; 
-                },
-                function() { 
-                    // cancelled save
-                    inMiddleOfSave = false; 
-                });        
-        }
-
-	e.cancelBubble = true;
-	if (e.stopPropagation) { e.stopPropagation(); }
-	e.returnValue = false;
-	if (e.preventDefault) { e.preventDefault(); }
-	if (! e.preventDefault) {
-	    // IE-specific hack.
-	    e.keyCode = 0;
-	}
-	return false;
+            myEditor.save(
+                      function() { 
+                          // successful save
+                          inMiddleOfSave = false; 
+                      },
+                      function() {
+                          // failed save
+                          inMiddleOfSave = false; 
+                      },
+                      function() { 
+                          // cancelled save
+                          inMiddleOfSave = false; 
+                      });        
+          }
+          e.cancelBubble = true;
+          if (e.stopPropagation) { e.stopPropagation(); }
+          e.returnValue = false;
+          if (e.preventDefault) { e.preventDefault(); }
+          if (! e.preventDefault) {
+              // IE-specific hack.
+              e.keyCode = 0;
+          }
+          return false;
     }
 
     if (e.keyCode === F5_KEYCODE) {
-	myEditor.run();
-	e.cancelBubble = true;
-	if (e.stopPropagation) { e.stopPropagation(); }
-	e.returnValue = false;
-	if (e.preventDefault) { e.preventDefault(); }
-	if (! e.preventDefault) {
-	    // IE-specific hack.
-	    e.keyCode = 0;
-	}
-	return false;
+      myEditor.run();
+      e.cancelBubble = true;
+      if (e.stopPropagation) { e.stopPropagation(); }
+      e.returnValue = false;
+      if (e.preventDefault) { e.preventDefault(); }
+      if (! e.preventDefault) {
+          // IE-specific hack.
+          e.keyCode = 0;
+      }
+      return false;
+    }
+    if (e.keyCode === ZOOMIN_KEYCODE &&
+        (plt.wescheme.BrowserDetect.OS==="Mac" ? e.metaKey : e.ctrlKey)) {
+      var size = parseInt(document.getElementById('middle').style.fontSize) || 10;
+      document.getElementById('middle').style.fontSize=(size+1)+'pt';
+      return false;
+    }
+    if (e.keyCode === ZOOMOUT_KEYCODE &&
+        (plt.wescheme.BrowserDetect.OS==="Mac" ? e.metaKey : e.ctrlKey)) {
+      var size = parseInt(document.getElementById('middle').style.fontSize) || 10;
+      document.getElementById('middle').style.fontSize=(size-1)+'pt';
+      return false;
     }
 
     // Special case: we want to intercept all backspaces that hit the
     // document body, and prevent us from going back in history.
     if (e.target === document.body) {
-	if (e.keyCode === BACKSPACE_KEYCODE) {
-	    e.cancelBubble = true;
-	    if (e.stopPropagation) { e.stopPropagation(); }
-	    e.returnValue = false;
-	    if (e.preventDefault) { e.preventDefault(); }
-	    return false;
-	}
+      if (e.keyCode === BACKSPACE_KEYCODE) {
+          e.cancelBubble = true;
+          if (e.stopPropagation) { e.stopPropagation(); }
+          e.returnValue = false;
+          if (e.preventDefault) { e.preventDefault(); }
+          return false;
+      }
+      return true;
     }
-
-    return true;
 }
-
-
 
