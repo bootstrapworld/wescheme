0f163f828814fa2c152940d5eb9a53019e4a3e67
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2015-08-26 17:35:06 -0400

    factor out sanitation of whitespace

--- war-src/js/compiler/annotateCircles.js
+++ war-src/js/compiler/annotateCircles.js
@@ -4,31 +4,11 @@ goog.require("plt.compiler.literal");
 goog.require("plt.compiler.symbolExpr");
 goog.require("plt.compiler.Program");
 goog.require("plt.compiler.couple");
-goog.require("plt.compiler.ifExpr");
-goog.require("plt.compiler.beginExpr");
-goog.require("plt.compiler.letExpr");
-goog.require("plt.compiler.letStarExpr");
-goog.require("plt.compiler.letrecExpr");
-goog.require("plt.compiler.localExpr");
 goog.require("plt.compiler.andExpr");
 goog.require("plt.compiler.orExpr");
 goog.require("plt.compiler.condExpr");
-goog.require("plt.compiler.caseExpr");
 goog.require("plt.compiler.lambdaExpr");
-goog.require("plt.compiler.quotedExpr");
-goog.require("plt.compiler.unquotedExpr");
-goog.require("plt.compiler.quasiquotedExpr");
-goog.require("plt.compiler.unquoteSplice");
 goog.require("plt.compiler.callExpr");
-goog.require("plt.compiler.whenUnlessExpr");
-goog.require("plt.compiler.defFunc");
-goog.require("plt.compiler.defVar");
-goog.require("plt.compiler.defVars");
-goog.require("plt.compiler.defStruct");
-goog.require("plt.compiler.requireExpr");
-goog.require("plt.compiler.provideStatement");
-goog.require("plt.compiler.unsupportedExpr");
-goog.require("plt.compiler.throwError");
 
 // if not defined, declare the compiler object as part of plt
 window.plt   = window.plt   || {};
@@ -42,30 +22,11 @@ plt.compiler = plt.compiler || {};
  var symbolExpr       = plt.compiler.symbolExpr;
  var Program          = plt.compiler.Program;
  var couple           = plt.compiler.couple;
- var ifExpr           = plt.compiler.ifExpr;
- var beginExpr        = plt.compiler.beginExpr;
- var letExpr          = plt.compiler.letExpr;
- var letStarExpr      = plt.compiler.letStarExpr;
- var letrecExpr       = plt.compiler.letrecExpr;
- var localExpr        = plt.compiler.localExpr;
  var andExpr          = plt.compiler.andExpr;
  var orExpr           = plt.compiler.orExpr;
  var condExpr         = plt.compiler.condExpr;
- var caseExpr         = plt.compiler.caseExpr;
  var lambdaExpr       = plt.compiler.lambdaExpr;
- var quotedExpr       = plt.compiler.quotedExpr;
- var unquotedExpr     = plt.compiler.unquotedExpr;
- var quasiquotedExpr  = plt.compiler.quasiquotedExpr;
- var unquoteSplice    = plt.compiler.unquoteSplice;
  var callExpr         = plt.compiler.callExpr;
- var whenUnlessExpr   = plt.compiler.whenUnlessExpr;
- var defFunc          = plt.compiler.defFunc;
- var defVar           = plt.compiler.defVar;
- var defVars          = plt.compiler.defVars;
- var defStruct        = plt.compiler.defStruct;
- var requireExpr      = plt.compiler.requireExpr;
- var provideStatement = plt.compiler.provideStatement;
- var unsupportedExpr  = plt.compiler.unsupportedExpr;
 
  //////////////////////////////////////////////////////////////////////////////
  // ANNOTATE CIRCLES ////////////////////////////////////////////////////////////////
@@ -74,6 +35,7 @@ plt.compiler = plt.compiler || {};
  // assign widgets representing circles of evaluation to tokens in CM
  function programToCircles(programs, cm){
     console.log('refreshing');
+    var wrapper = cm.getWrapperElement();
     cm.circleIndices = {};
     // clear circles
     cm.getAllMarks().filter(function(m){return m._circles;}).forEach(function(m){m.clear()});
@@ -111,7 +73,7 @@ plt.compiler = plt.compiler || {};
     };
  
     // avoid addEventListener, since it will register duplicate handlers
-    cm.getWrapperElement().onkeydown = handleKey;
+    wrapper.onkeydown = handleKey;
     function handleKey(e){
       var active = document.activeElement
       if(e.which===8 && isSexp(active)){    // DELETE
@@ -145,11 +107,42 @@ plt.compiler = plt.compiler || {};
       cm.markText(circle.from, circle.to,
                  {replacedWith: circle
                  , handleMouseEvents: false
-                 , _circles: true});    });
+                 , _circles: true});
+    });
  
-    function setCursor(startChar, offset){
-      var node = cm.circleIndices[startChar];
-      node.focus(); return node;
+    // if a node is modified in-place, update the location of ancestors and siblings
+    function inPlaceChange(node){
+      // given a node and delta, move it and all its' children by that delta
+      function shiftNode(node, delta){
+        if(node.className==="lineBreak") return;
+        node.from.ch += delta; node.to.ch += delta;
+        for(var i = 0; i<node.children.length && node.children[i].from.line===line; i++){
+          shiftNode(node.children[i], delta); };
+      }
+      // any following siblings on the same line should be *shifted* by delta
+      function shiftSiblings(node, delta){
+        for(var sib = node.nextSibling; sib && sib.from && (sib.from.line===line);
+            sib = sib.nextSibling){ shiftNode(sib, delta); }
+      }
+      // If an Ancestor ends on the same line, it should be *extended* by delta,
+      // with its' siblings shifted by the same amount
+      function extendAncestors(node, delta){
+        for(var parent = node.parentNode; isCircle(parent) && parent.to.line===line;
+            parent = parent.parentNode){ parent.to.ch += delta; shiftSiblings(parent, delta); }
+      }
+      var from = node.from, to = node.to, line = to.line,
+          delta = node.innerText.length - ((node.to.ch) - (node.from.ch));
+      node.to.ch = node.to.ch + delta; // adjust the node's location information
+      shiftSiblings(node, delta);      // shift any siblings over
+      extendAncestors(node, delta);    // extend any ancestors
+    }
+ 
+    function sanitizeWhitespace(node, txt){
+      var start = cm.indexFromPos(node.from),
+          end   = cm.indexFromPos(node.to),
+          prev  = cm.getRange(cm.posFromIndex(start-1), node.from),
+          next  = cm.getRange(node.to, cm.posFromIndex(end+1));
+      return  (/\s|[\(\[\{]/.test(prev)? "":" ")+ txt.trim() +(/\s|[\)\]\}]/.test(next)? "":" ");
     }
  
     // select the highlighted node.
@@ -164,6 +157,7 @@ plt.compiler = plt.compiler || {};
       node = getNodeFromStoppedEvent(e) || node;
       node.contentEditable = "true";
       node.onblur = function(){saveEdit(node)};
+      node.ondblclick = null; // allow dblclick to work normally
       node.onkeydown = function(e){
         e.stopPropagation();
         e.codemirrorIgnore = true;
@@ -182,10 +176,7 @@ plt.compiler = plt.compiler || {};
       node.onkeydown = null;
       node.contentEditable = "false";
       node.classList.remove('editing');
-      var text = node.innerText;
-      // add spaces if we're inserting new text
-      if(isWhitespace(node)){ text = " "+node.innerText+" "; }
-      cm.replaceRange(text, node.from, node.to);
+      cm.replaceRange(sanitizeWhitespace(node, node.innerText), node.from, node.to);
     }
  
     function handleDragStart(e){// make draggable things translucent
@@ -215,7 +206,7 @@ plt.compiler = plt.compiler || {};
     }
     // if we've dropped one element into a different one, perform the CM modifications
     function handleDrop(e) {
-      e.codemirrorIgnore = true;
+      e.codemirrorIgnore = true; e.preventDefault();
       var node = getNodeFromStoppedEvent(e) || node, active = document.activeElement;
       if(!node.location){
         var pos = cm.coordsChar({left: e.pageX, top: e.pageY});
@@ -223,24 +214,12 @@ plt.compiler = plt.compiler || {};
       }
       node.classList.remove('insert', 'replace');
       if (!active.contains(node)) {
-        if(active === cm.getInputField()){ // for dropping into the parent
-          var idx = cm.getValue().length+1;
-          cm.replaceRange(text, node.from, node.to);
-          active.location = {startChar: idx, endChar: idx};
-          return;
-        }
         var text     = e.dataTransfer.getData('text');
-        // if we're inserting new text, decide if we need extra spaces
-        if(isWhitespace(node)){
-          var start = cm.indexFromPos(node.from),
-              end   = cm.indexFromPos(node.to),
-              prev  = cm.getRange(cm.posFromIndex(start-1), node.from),
-              next  = cm.getRange(node.to, cm.posFromIndex(end+1));
-          var spaceOrBracket = /\s|[\(\[\{\)\}\]]/;
-          text =  (spaceOrBracket.test(prev)? "" : " ") +
-                  e.dataTransfer.getData('text') +
-                  (spaceOrBracket.test(next)? "" : " ");
+        if(node.classList.contains('CodeMirror-line')){ // for dropping into the parent
+          var pos = cm.coordsChar({left: e.pageX, top: e.pageY});
+          node.from = node.to = pos;
         }
+        text = sanitizeWhitespace(node, text);
         // Modify the src or dest first, depending on which comes earlier in the document
         // Be sure to combine both operations into one, so undo works properly
         cm.operation(function(){
@@ -263,13 +242,12 @@ plt.compiler = plt.compiler || {};
     });
             
     // Assign Event Handlers (avoid addEventListener, which allows duplicates)
-    var whitespace  = document.querySelectorAll('.cm-whitespace'),
-        sexpElts    = document.querySelectorAll('.value, .sexp>*:nth-child(2), .sexp'),
-        draggable   = document.querySelectorAll('.value, .sexp>*:nth-child(2), .sexp'),
-        dragTargets = document.querySelectorAll('.value, .sexp>*:nth-child(2), .sexp, .cm-whitespace'),
-        dropTargets = document.querySelectorAll('.value, .sexp>*:nth-child(2), .cm-whitespace'),
-        editable    = document.querySelectorAll('.value'),
-        parent = cm.getWrapperElement();
+    var whitespace  = wrapper.querySelectorAll('.cm-whitespace'),
+        sexpElts    = wrapper.querySelectorAll('.value, .sexp>*:nth-child(2), .sexp'),
+        draggable   = wrapper.querySelectorAll('.value, .sexp>*:nth-child(2), .sexp'),
+        dragTargets = wrapper.querySelectorAll('.value, .sexp>*:nth-child(2), .sexp, .cm-whitespace'),
+        dropTargets = wrapper.querySelectorAll('.value, .sexp>*:nth-child(2), .cm-whitespace'),
+        editable    = wrapper.querySelectorAll('.value');
             
     // editable things can be edited on dblclick
     [ ].forEach.call(editable,  function (elt){elt.ondblclick = startEdit;});
@@ -285,11 +263,8 @@ plt.compiler = plt.compiler || {};
     [ ].forEach.call(dropTargets, function (elt){
        elt.ondragenter = colorTarget; elt.ondragleave = unColorTarget;
        elt.ondrop = handleDrop; });
-            
-    parent.addEventListener("dragenter", colorTarget,     false);
-    parent.addEventListener("dragover",  handleDragOver,  false);
-    parent.addEventListener("dragleave", unColorTarget,   false);
-    parent.addEventListener("drop",      handleDrop,      false);
+    // allow dropping into the CM element
+    cm.on("drop", function(cm, e){ e.CodeMirrorIgnore = true; handleDrop(e); });
  
   }, 400);
  }
@@ -306,7 +281,9 @@ plt.compiler = plt.compiler || {};
     br.style.height = "0"; br.className = "lineBreak";
     parent.appendChild(makeWhitespace(startCh, lines[0], cm));
     for(var i=1; i<lines.length; i++){
-      parent.appendChild(br.cloneNode(true));
+      var lineBreak = br.cloneNode(true);
+      lineBreak.from = lineBreak.to = child.from;
+      parent.appendChild(lineBreak);
       parent.appendChild(makeWhitespace(startCh, lines[i], cm));
       startCh+=lines[i].length+1;
     }
@@ -354,11 +331,17 @@ plt.compiler = plt.compiler || {};
     expression.to   = cm.posFromIndex(location.endChar);
     lParen.className = "lParen";        rParen.className = "rParen";
     lParen.location = location.start(); rParen.location = location.end();
+    lParen.from = cm.posFromIndex(location.startChar);
+    lParen.to = cm.posFromIndex(location.startChar+1);
     lParen.appendChild(document.createTextNode(cm.getTokenAt(startPos).string));
     rParen.appendChild(document.createTextNode(cm.getTokenAt(endPos).string));
+    rParen.from = cm.posFromIndex(location.endChar);
+    rParen.to = cm.posFromIndex(location.endChar+1);
     expression.appendChild(lParen);
     operator.location = func? func.location :
                 {startChar: location.startChar+1, endChar: location.endChar-1};
+    operator.from = cm.posFromIndex(func.location.startChar);
+    operator.to = cm.posFromIndex(func.location.endChar);
     var funcValue = func? func.toCircles(cm) :
                     makeValue(" ", "cm-whitespace", operator.location, cm);
     funcValue.location = operator.location;
@@ -421,4 +404,5 @@ plt.compiler = plt.compiler || {};
       console.log("Converted toCircles in "+(Math.floor(end-start))+"ms");
     }
   };
+
 })();
