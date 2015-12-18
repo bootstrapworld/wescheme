bd53667ed52389208864f95635cdfb690910ae6c
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2015-12-18 13:59:14 -0500

    allow () to parse, and throw errors in the desugaring stage (needed for blocks work). Bump v number

--- war-src/js/compiler/parser.js
+++ war-src/js/compiler/parser.js
@@ -994,9 +994,11 @@ plt.compiler = plt.compiler || {};
                     isLiteral(sexp) ? sexp :
                     isSymbolEqualTo("quote", sexp) ? new quotedExpr(sexp) :
                     isSymbolEqualTo("empty", sexp) ? new callExpr(new symbolExpr("list"), []) :
-      throwError(new types.Message([new types.ColoredPart("( )", sexp.location)
+                    new callExpr(null, []);
+/*      throwError(new types.Message([new types.ColoredPart("( )", sexp.location)
                                     , ": expected a function, but nothing's there"])
                  , sexp.location);
+*/
    singleton.location = sexp.location;
    return singleton;
   }
