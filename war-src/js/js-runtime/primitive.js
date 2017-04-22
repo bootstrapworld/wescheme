4fab440b199f228d27995d33d40ed7dec58292b9
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2017-04-21 22:53:52 -0400

    .

--- war-src/js/js-runtime/primitive.js
+++ war-src/js/js-runtime/primitive.js
@@ -4589,11 +4589,13 @@ PRIMITIVES['star'] =
 			  if (colorDb.get(color)) {
 			      color = colorDb.get(color);
 			  }
-			  return world.Kernel.polygonImage(jsnums.toFixnum(sideLength), 
-							   jsnums.toFixnum(5), 
-							   jsnums.toFixnum(2), 
-							   mode.toString(), 
-							   color);
+			  return world.Kernel.polygonImage(
+			  					jsnums.toFixnum(sideLength), 
+							   	jsnums.toFixnum(5), 
+							   	jsnums.toFixnum(2), 
+							   	mode.toString(), 
+							   	color,
+							   	"star");
 		      })]);
 
 
